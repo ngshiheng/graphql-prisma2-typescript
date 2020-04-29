37a47853b46e7285cba7061079bff4c818eb5402
Jerry Ng <ngshiheng@gmail.com>
2020-04-29 00:46:37 +0000

    Add docker login

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ TAG=$(date +%y.%m.%d)-$(git rev-parse --short HEAD)
 
 echo "🐳 Building docker image for: $DOCKER_IMAGE_NAME:$TAG"
 docker build -t "$DOCKER_IMAGE_NAME:$TAG" .
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker images
 docker push "$DOCKER_IMAGE_NAME:$TAG"
